b286670b550e15da916182a5bd41f55a7df08d53
MediaSFU <157974639+MediaSFU@users.noreply.github.com>
2025-09-15 23:15:01 -0600

    mediasoup-client update for license issues.

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  plugins: ["@typescript-eslint", "react", "react-refresh"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "prefer-const": "off",
+    "react/react-in-jsx-scope": "off",
+    "no-inner-declarations": "off",
+    "@typescript-eslint/no-unused-expressions": "off"
+  }
+};
